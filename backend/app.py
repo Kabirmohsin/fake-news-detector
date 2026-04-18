f7425e4a7da3b6f6be51e98f676c352391db9ff5
Kabir Mohsin <kabirmohsin892@gmail.com>
2026-04-18 13:25:52 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -43,4 +43,8 @@ def predict():
         "result": "Real News" if result == 1 else "Fake News"
     })
 
-app.run()
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
